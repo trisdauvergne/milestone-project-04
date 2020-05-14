521b6b25927222ec0b81acaa0472aa1f02c1a5a8
trisdauvergne <tristan.dauvergne@gmail.com>
2020-05-14 16:28:29 +0000

    200514 - 18:28 feat: deploying

--- planchest/settings.py
+++ planchest/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'ikij3rn1q)=n@mx_j$7^y_er#*#i_%k#z$a-((r%wdm@#*=2-v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['planchest-ms4.herokuapp.com']
 
 
 # Application definition
