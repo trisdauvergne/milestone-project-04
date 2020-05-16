bfb744baad0b96229de9171b1e1225b4ef88f6fe
trisdauvergne <tristan.dauvergne@gmail.com>
2020-05-16 13:25:07 +0000

    200516 - 15:24 feat: added messages back in for when items are added to bag

--- bag/views.py
+++ bag/views.py
@@ -21,9 +21,10 @@ def add_print_to_bag(request, print_id):
 
     if print_id in list(bag.keys()):
         bag[print_id] += quantity
+        print('test print')
+        messages.success(request, f'Product was Updated!')
     else:
         bag[print_id] = quantity
-        # messages.success(request, f'Added to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -10,5 +10,17 @@ class StripeWH_Handler:
     def handle_event(self, event):
         """ Generic/unknown/unexpected WH """
         return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """ Every time a payment_intent.succeeded webhook """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
+    
+    def handle_payment_intent_failed(self, event):
+        """ Every time a payment_intent.payment_failed webhook """
+        return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
