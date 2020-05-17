30ae06d31c035277bfbb4017407866273c07c49c
trisdauvergne <tristan.dauvergne@gmail.com>
2020-05-17 13:05:40 +0000

    200517 - 15:05 feat: added function to sort designer names alphabeitcally on all designers page

--- designers/urls.py
+++ designers/urls.py
@@ -3,5 +3,6 @@ from .import views
 
 urlpatterns = [
     path('', views.all_designers, name='all_designers'),
+    path('designers-alphabetical', views.all_designers_alphabetical, name='all_designers_alphabetical'),
     path('designer-detail/<designer_id>/', views.individual_designer, name='individual_designer'),
 ]
--- designers/views.py
+++ designers/views.py
@@ -28,6 +28,27 @@ def all_designers(request):
                   context)
 
 
+def all_designers_alphabetical(request):
+    checked_designers = []
+    all_designers = RegisteredUserProfile.objects.all()
+    designers_by_name = all_designers.order_by('last_name')
+    for designer in designers_by_name:
+        complete = all([designer.first_name,
+                        designer.last_name,
+                        designer.bio,
+                        designer.country,
+                        designer.register_as_designer])
+        if complete:
+            checked_designers.append(designer)
+    context = {
+        'designers': checked_designers,
+    }
+
+    return render(request,
+                  'designers/all_designers_alphabetical.html',
+                  context)
+
+
 def individual_designer(request, designer_id):
     """ A view to return a page focusing on 1 x designer """
     the_designer = RegisteredUserProfile.objects.get(id=designer_id)
