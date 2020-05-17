b06d76ed5941bec2b706bcf0437fdb4a8887c7eb
trisdauvergne <tristan.dauvergne@gmail.com>
2020-05-17 14:49:51 +0000

    200517 - 16:49 test: tested the get_large_print view in prints app

--- prints/test_views.py
+++ prints/test_views.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from .models import Print
-# from profiles.models import RegisteredUserProfile
-# from django.contrib.auth.models import User
+from profiles.models import RegisteredUserProfile
+from django.contrib.auth.models import User
 
 
 class TestPrintsView(TestCase):
@@ -21,15 +21,45 @@ class TestPrintsView(TestCase):
     #     self.assertTemplateUsed(response, 'prints/all_prints_designer.html')
 
     def test_get_large_print(self):
-        the_print = Print.objects.create(designer_id=99,
-                                         price=25)
+    # Create the user w/o specifying the id. Django will give it one
+        the_user = User.objects.create()
+        # Same for designer. The user field is a FK to an actual user
+        the_designer = RegisteredUserProfile.objects.create(user=the_user,
+                                                            register_as_designer=True,
+                                                            register_as_customer=False,
+                                                            first_name='test',
+                                                            last_name='test',
+                                                            bio='test',
+                                                            country='US')
+        # And the designer field here is a FK to an actual designer
+        the_print = Print.objects.create(designer=the_designer,
+                                            price=25,
+                                            image='test.jpg')
         response = self.client.get(f'/prints/large-print/{the_print.id}/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'prints/large_print.html')
 
     # def test_get_edit_print(self):
-    #     the_print = Print.objects.create(designer_id=1,
-    #                                      price=25)
-    #     response = self.client.get(f'/prints/large-print/{the_print.id}/')
+    #     # Create the user w/o specifying the id. Django will give it one
+    #     the_user = User.objects.create()
+    #     # Same for designer. The user field is a FK to an actual user
+    #     the_designer = RegisteredUserProfile.objects.create(user=the_user,
+    #                                                         register_as_designer=True,
+    #                                                         register_as_customer=False,
+    #                                                         first_name='test',
+    #                                                         last_name='test',
+    #                                                         bio='test',
+    #                                                         country='US')
+    #     # And the designer field here is a FK to an actual designer
+    #     the_print = Print.objects.create(designer=the_designer,
+    #                                         price=25,
+    #                                         image='test.jpg')
+        
+    #     response = self.client.get(f'/prints/edit-print/{the_print.id}/')
     #     self.assertEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(response, 'prints/large_print.html')
+    #     self.assertTemplateUsed(response, 'prints/edit_print.html') 
+
+    # def test_add_print(self):
+    #     response = self.client.get('/prints/add-print/')
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertTemplateUsed(response, 'prints/add_print.html')
