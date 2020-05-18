31fc61016b942c4bb4cbd564cbaac2a4471e898b
trisdauvergne <tristan.dauvergne@gmail.com>
2020-05-18 10:10:23 +0000

    200517 - 12:10 test: tested add print page loads in test_views prints app

--- prints/test_views.py
+++ prints/test_views.py
@@ -1,7 +1,11 @@
+from django.shortcuts import get_object_or_404
 from django.test import TestCase
 from .models import Print
 from profiles.models import RegisteredUserProfile
 from django.contrib.auth.models import User
+from .forms import UploadPrintForm
+from django.contrib.auth.decorators import login_required
+# from django.contrib.auth import get_user_model
 
 
 class TestPrintsView(TestCase):
@@ -40,28 +44,39 @@ class TestPrintsView(TestCase):
     #     self.assertEqual(response.status_code, 200)
     #     self.assertTemplateUsed(response, 'prints/large_print.html')
 
-    def test_get_edit_print(self):
-        # Create the user w/o specifying the id. Django will give it one
-        the_user = User.objects.create()
-        # Same for designer. The user field is a FK to an actual user
-        the_designer = RegisteredUserProfile.objects.create(user=the_user,
-                                                            register_as_designer=True,
-                                                            register_as_customer=False,
-                                                            first_name='test',
-                                                            last_name='test',
-                                                            bio='test',
-                                                            country='US')
-        the_designer.save()
-        # And the designer field here is a FK to an actual designer
-        the_print = Print.objects.create(designer=the_designer,
-                                            price=25,
-                                            image='test.jpg')
-        
-    #     response = self.client.get(f'/prints/edit-print/{the_print.id}/')
+    # @login_required
+    # def test_get_edit_print(self):
+    #     # Create the user w/o specifying the id. Django will give it one
+    #     the_user = User.objects.create()
+    #     print(the_user)
+    #     # Same for designer. The user field is a FK to an actual user
+    #     the_designer = RegisteredUserProfile.objects.create(user=the_user,
+    #                                                         register_as_designer=True,
+    #                                                         register_as_customer=False,
+    #                                                         first_name='test',
+    #                                                         last_name='test',
+    #                                                         bio='test',
+    #                                                         country='US')
+    #     # And the designer field here is a FK to an actual designer
+    #     the_designer.save()
+    #     the_print = Print.objects.create(designer=the_designer,
+    #                                      price=25,
+    #                                      image='test.jpg')
+    #     the_print.save()
+    #     response = self.client.get(f'/prints/edit-print/{the_print.id}')
     #     self.assertEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(response, 'prints/edit_print.html') 
+    #     self.assertTemplateUsed(response, 'prints/edit_print.html')
 
-    # def test_add_print(self):
-    #     response = self.client.get('/prints/add-print/')
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(response, 'prints/add_print.html')
+    def test_add_print(self):
+        # The user
+        User.objects.create(username='temporary',
+                            email='temporary@gmail.com',
+                            password='secret')
+        # Logging the user in
+        self.client.login(username='temporary',
+                          password='secret')
+        # The page, following a redirect
+        response = self.client.get('/prints/add-print/',
+                                   follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(template_name='prints/add_print.html')
