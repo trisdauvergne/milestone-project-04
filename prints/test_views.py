12907343bd9471400c24124dbcca32d0024f6f4a
trisdauvergne <tristan.dauvergne@gmail.com>
2020-05-18 07:34:24 +0000

    200516 - 09:34 test: tested get_edit_print view in prints app

--- prints/test_views.py
+++ prints/test_views.py
@@ -20,8 +20,28 @@ class TestPrintsView(TestCase):
     #     self.assertEqual(response.status_code, 200)
     #     self.assertTemplateUsed(response, 'prints/all_prints_designer.html')
 
-    def test_get_large_print(self):
-    # Create the user w/o specifying the id. Django will give it one
+    # def test_get_large_print(self):
+    # # Create the user w/o specifying the id. Django will give it one
+    #     the_user = User.objects.create()
+    #     # Same for designer. The user field is a FK to an actual user
+    #     the_designer = RegisteredUserProfile.objects.create(user=the_user,
+    #                                                         register_as_designer=True,
+    #                                                         register_as_customer=False,
+    #                                                         first_name='test',
+    #                                                         last_name='test',
+    #                                                         bio='test',
+    #                                                         country='US')
+    #     the_designer.save()
+    #     # And the designer field here is a FK to an actual designer
+    #     the_print = Print.objects.create(designer=the_designer,
+    #                                         price=25,
+    #                                         image='test.jpg')
+    #     response = self.client.get(f'/prints/large-print/{the_print.id}/')
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertTemplateUsed(response, 'prints/large_print.html')
+
+    def test_get_edit_print(self):
+        # Create the user w/o specifying the id. Django will give it one
         the_user = User.objects.create()
         # Same for designer. The user field is a FK to an actual user
         the_designer = RegisteredUserProfile.objects.create(user=the_user,
@@ -36,25 +56,6 @@ class TestPrintsView(TestCase):
         the_print = Print.objects.create(designer=the_designer,
                                             price=25,
                                             image='test.jpg')
-        response = self.client.get(f'/prints/large-print/{the_print.id}/')
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'prints/large_print.html')
-
-    # def test_get_edit_print(self):
-    #     # Create the user w/o specifying the id. Django will give it one
-    #     the_user = User.objects.create()
-    #     # Same for designer. The user field is a FK to an actual user
-    #     the_designer = RegisteredUserProfile.objects.create(user=the_user,
-    #                                                         register_as_designer=True,
-    #                                                         register_as_customer=False,
-    #                                                         first_name='test',
-    #                                                         last_name='test',
-    #                                                         bio='test',
-    #                                                         country='US')
-    #     # And the designer field here is a FK to an actual designer
-    #     the_print = Print.objects.create(designer=the_designer,
-    #                                         price=25,
-    #                                         image='test.jpg')
         
     #     response = self.client.get(f'/prints/edit-print/{the_print.id}/')
     #     self.assertEqual(response.status_code, 200)
