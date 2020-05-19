b56a465fba9593b52f9c21039e7b97e0f5c6d822
trisdauvergne <tristan.dauvergne@gmail.com>
2020-05-19 11:13:11 +0000

    200519 - 13:12 feat: fixed if statement in navbar to only show add work button if user is registered as a designer

--- checkout/views.py
+++ checkout/views.py
@@ -121,7 +121,7 @@ def checkout(request):
 
 def checkout_success(request, order_number):
     """ Handle successful checkouts """
-    save_info = request.session.get('save_info')
+    # save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
     messages.success(request, f'Order successfully processed! \
     Your order number is {order_number}. A confirmation email \
--- home/views.py
+++ home/views.py
@@ -7,6 +7,7 @@ def index(request):
     """ A view to return the landing 'index' page """
 
     user = request.user
+
     # 1. Check if all information.
     # 2. If not, redirect to profile page. 
     return render(request, 'home/index.html')
