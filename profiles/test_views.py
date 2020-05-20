5a41529873f6820612dfc448749ab95bac5cae34
trisdauvergne <tristan.dauvergne@gmail.com>
2020-05-20 21:46:11 +0000

    200520 - 23:45 maint: html validated the bag and checkout templates

--- profiles/test_views.py
+++ profiles/test_views.py
@@ -1,11 +1,12 @@
 from django.test import TestCase
-from django.test import Client
-from django.contrib.auth import login
-from django.http import HttpRequest
-
+# from django.test import Client
+# from django.contrib.auth import login
+# from django.http import HttpRequest
 
 from django.contrib.auth.models import User
+from profiles.models import RegisteredUserProfile
 
+from profiles.forms import RegisteredUserProfileForm
 
 class TestProfilesView(TestCase):
     def test_load_create_profile_page(self):
@@ -36,7 +37,7 @@ class TestProfilesView(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(template_name='profiles/order_history.html')
 
-    def login_user(self):
+    def test_login_user(self):
         """ Check logged in user """
         user = User.objects.create_user(username='temporary',
                                         email='temp@gmail.com',
@@ -46,5 +47,22 @@ class TestProfilesView(TestCase):
                                   password='wrong')
         self.assertFalse(login)
 
+    def test_get_designer_details_in_form(self):
+        """ Getting logged in designer's details """
+        # Create the user
+        the_user = User.objects.create()
 
+        # Create a designer
+        the_designer = RegisteredUserProfile.objects.create(user=the_user,
+                                                            register_as_designer=True,
+                                                            register_as_customer=False,
+                                                            first_name='test',
+                                                            last_name='test',
+                                                            bio='test',
+                                                            country='US')
 
+        # The form to show the details
+        profile_form = RegisteredUserProfileForm(instance=the_designer)
+
+        # When sent
+        self.fail(profile_form.as_p())
